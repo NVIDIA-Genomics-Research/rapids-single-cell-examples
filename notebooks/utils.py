99b0c05c40e850d66334d9eede06788e5269a9b9
Rajesh Ilango <rilango@nvidia.com>
2021-03-16 21:08:25 -0700

    Bugfix: Batch filter_cells and filter_genes feature.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,6 +1,14 @@
 import math
+import h5py
 import numpy as np
+import cupy as cp
+import cudf
 import scipy
+import dask
+
+from cuml.dask.common.part_utils import _extract_partitions
+from cuml.common.memory_utils import with_cupy_rmm
+
 
 def pca(adata, n_components=50, train_ratio=0.35, n_batches=50, gpu=False):
 
@@ -38,7 +46,7 @@ def pca(adata, n_components=50, train_ratio=0.35, n_batches=50, gpu=False):
         import numpy as cp
 
     pca = PCA(n_components=n_components).fit(adata.X[:train_size])
-    
+
     embeddings = cp.zeros((adata.X.shape[0], n_components))
     batch_size = int(embeddings.shape[0] / n_batches)
     for batch in range(n_batches):
@@ -49,7 +57,7 @@ def pca(adata, n_components=50, train_ratio=0.35, n_batches=50, gpu=False):
             end_idx = adata.X.shape[0]
 
         embeddings[start_idx:end_idx,:] = cp.asarray(pca.transform(adata.X[start_idx:end_idx]))
-        
+
     if gpu:
         embeddings = embeddings.get()
 
@@ -64,14 +72,14 @@ def tf_idf(filtered_cells):
     Note that the 1 / sum_peaks(i) term isn't included in the standard NLP form of tf-idf, but other single-cell work uses it.
     '''
     inv_sums = 1 / np.array(filtered_cells.sum(axis=1)).ravel()
-    
+
     peak_counts = np.array((filtered_cells > 0).sum(axis=0)).ravel()
     log_inv_peak_freq = np.log1p(filtered_cells.shape[0] / peak_counts)
-    
+
     normalized = filtered_cells.multiply(inv_sums[:, np.newaxis])
     normalized = normalized.multiply(log_inv_peak_freq[np.newaxis, :])
     normalized = scipy.sparse.csr_matrix(normalized)
-    
+
     return normalized
 
 
@@ -80,16 +88,16 @@ def logtf_idf(filtered_cells, pseudocount=10**5):
     Input: 2D numpy.ndarray or 2D sparse matrix with X[i, j] = (binary or continuous) read count for cell i, peak j
     Output: Normalized matrix, where Xp[i, j] = X[i, j] * log(1 + pseudocount/sum_peaks(i)) * log(1 + N_cells/N_cells with peak j)
     Pseudocount should be chosen as a similar order of magnitude as the mean number of reads per cell.
-    ''' 
+    '''
     log_inv_sums = np.log1p(pseudocount / np.array(filtered_cells.sum(axis=1)).ravel())
-    
+
     peak_counts = np.array((filtered_cells > 0).sum(axis=0)).ravel()
     log_inv_peak_freq = np.log1p(filtered_cells.shape[0] / peak_counts)
-    
+
     normalized = filtered_cells.multiply(log_inv_sums[:, np.newaxis])
     normalized = normalized.multiply(log_inv_peak_freq[np.newaxis, :])
     normalized = scipy.sparse.csr_matrix(normalized)
-    
+
     return normalized
 
 
@@ -121,3 +129,113 @@ def filter_peaks(adata, n_top_peaks):
     frequent_peak_idxs = np.argsort(peak_frequency)
     use = frequent_peak_idxs[-n_top_peaks : ]
     return adata[:, use]
+
+
+@with_cupy_rmm
+def sum_csr_matrix(csr_matrix, client, axis=0):
+    '''
+    Implements sum operation for dask array when the backend is cupy sparse csr matrix
+    '''
+
+    client = dask.distributed.default_client()
+
+    def __sum(x):
+        return x.sum(axis=axis)
+
+    parts = client.sync(_extract_partitions, csr_matrix)
+    futures = [client.submit(__sum,
+                             part,
+                             workers=[w],
+                             pure=False)
+               for w, part in parts]
+    objs = []
+    for i in range(len(futures)):
+        obj = dask.array.from_delayed(futures[i],
+                                      shape=futures[i].result().shape,
+                                      dtype=cp.float32)
+        objs.append(obj)
+    return dask.array.concatenate(objs, axis=axis)
+
+
+def read_with_filter(client,
+                     sample_file,
+                     min_genes_per_cell=200,
+                     max_genes_per_cell=6000,
+                     min_cells = 1,
+                     num_cells=None,
+                     batch_size=5000):
+    """
+    Reads an h5ad file and applies cell and geans count filter. Dask Array is
+    used allow partitioning the input file. This function supports multi-GPUs.
+    """
+
+    # Path in h5 file
+    _data = '/X/data'
+    _index = '/X/indices'
+    _indprt = '/X/indptr'
+    _genes = '/var/_index'
+    _barcodes = '/obs/_index'
+
+    @dask.delayed
+    def _read_partition_to_sparse_matrix(sample_file,
+                                         total_cols, batch_start, batch_end,
+                                         min_genes_per_cell=200,
+                                         max_genes_per_cell=6000):
+        with h5py.File(sample_file, 'r') as h5f:
+            indptrs = h5f[_indprt]
+            start_ptr = indptrs[batch_start]
+            end_ptr = indptrs[batch_end]
+
+            # Read all things data and index
+            sub_data = cp.array(h5f[_data][start_ptr:end_ptr])
+            sub_indices = cp.array(h5f[_index][start_ptr:end_ptr])
+
+            # recompute the row pointer for the partial dataset
+            sub_indptrs  = cp.array(indptrs[batch_start:(batch_end + 1)])
+            sub_indptrs = sub_indptrs - sub_indptrs[0]
+
+        # Reconstruct partial sparse array
+        partial_sparse_array = cp.sparse.csr_matrix(
+            (sub_data, sub_indices, sub_indptrs),
+            shape=(batch_end - batch_start, total_cols))
+
+        # TODO: Add barcode filtering here.
+        degrees = cp.diff(partial_sparse_array.indptr)
+        query = ((min_genes_per_cell <= degrees) & (degrees <= max_genes_per_cell))
+        return partial_sparse_array[query]
+
+
+    with h5py.File(sample_file, 'r') as h5f:
+        # Compute the number of cells to read
+        indptr = h5f[_indprt]
+        genes = cudf.Series(h5f[_genes], dtype=cp.dtype('object'))
+
+        total_cols = genes.shape[0]
+        max_cells = indptr.shape[0] - 1
+        if num_cells is not None:
+            max_cells = num_cells
+
+    dls = []
+    for batch_start in range(0, max_cells, batch_size):
+        dls.append(dask.array.from_delayed(
+                   (_read_partition_to_sparse_matrix)
+                   (sample_file,
+                    total_cols,
+                    batch_start,
+                    batch_start + min(batch_size, max_cells - batch_start),
+                    min_genes_per_cell=min_genes_per_cell,
+                    max_genes_per_cell=max_genes_per_cell),
+                   dtype=cp.float32,
+                   shape=(batch_size, total_cols)))
+
+    dask_sparse_arr=  dask.array.concatenate(dls)
+    print('cell cnt after cell filter', dask_sparse_arr.shape)
+
+    # Filter by genes (i.e. cell count per gene)
+    gene_wise_cell_cnt = sum_csr_matrix(dask_sparse_arr, client).compute().sum(axis=0)
+    query = gene_wise_cell_cnt >= min_cells
+
+    # Filter genes for var
+    genes = genes[query]
+    genes = genes.reset_index(drop=True)
+    return dask_sparse_arr.compute()[:, query], genes
