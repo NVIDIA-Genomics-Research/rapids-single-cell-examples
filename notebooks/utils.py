1dd70234c9daf629410fe71cd0dfd6160642b073
Avantika Lal <avantikalal1990@gmail.com>
2021-07-27 21:22:26 -0700

    np instead of cp array for utils.pca embeddings

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -32,14 +32,12 @@ def pca(adata, n_components=50, train_ratio=0.35, n_batches=50, gpu=False):
 
     if gpu:
         from cuml.decomposition import PCA
-        import cupy as cp
     else:
         from sklearn.decomposition import PCA
-        import numpy as cp
 
     pca = PCA(n_components=n_components).fit(adata.X[:train_size])
     
-    embeddings = cp.zeros((adata.X.shape[0], n_components))
+    embeddings = np.zeros((adata.X.shape[0], n_components))
     batch_size = int(embeddings.shape[0] / n_batches)
     for batch in range(n_batches):
         start_idx = batch * batch_size
@@ -48,10 +46,7 @@ def pca(adata, n_components=50, train_ratio=0.35, n_batches=50, gpu=False):
         if(adata.X.shape[0] - end_idx < batch_size):
             end_idx = adata.X.shape[0]
 
-        embeddings[start_idx:end_idx,:] = cp.asarray(pca.transform(adata.X[start_idx:end_idx]))
-        
-    if gpu:
-        embeddings = embeddings.get()
+        embeddings[start_idx:end_idx,:] = pca.transform(adata.X[start_idx:end_idx])
 
     adata.obsm["X_pca"] = embeddings
     return adata
